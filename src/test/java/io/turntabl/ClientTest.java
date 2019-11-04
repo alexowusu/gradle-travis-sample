b199a240cee2ee017c1d64e8b160c9bc56ec11b2
alexowusu <owusu3y@gmail.com>
2019-11-04 11:43:42 +0000

    incomp

---
+++ src/test/java/io/turntabl/ClientTest.java
@@ -0,0 +1,23 @@
+package io.turntabl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void getServiceLevel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTypeOfClient() {
+    }
+}
